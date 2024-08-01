cd5be8cb2066dd38c41ad3541c5cbce759d4e4a2
Jimmy <jimmyrss1102@gmail.com>
2024-08-02 07:48:35 +0800

    feat: complete the `set&get` of `npm` registry

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,30 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::process::Command;
+use tauri::api::process::restart;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn execute_cmd_command(cmd_command: &str) -> String {
+    let output = Command::new("cmd")
+        .args(&["/C", cmd_command])
+        .output()
+        .expect("failed");
+
+    // 将输出转换为字符串
+    let version = String::from_utf8_lossy(&output.stdout);
+    return version.to_string();
+}
+
+#[tauri::command]
+fn restart_tauri() {
+    restart(&tauri::Env::default());
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![execute_cmd_command, restart_tauri])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
+
+import 'tdesign-vue-next/es/style/index.css';
+
